b3259747a97a2a296804b99b63c2f8dafb0ab4e6
Steven Uriarte <54910978l@iespoblenou.org>
2024-12-03 09:05:09 +0100

    functionalities to LivingRoom

--- src/home/LivingRoom.java
+++ src/home/LivingRoom.java
@@ -1,4 +1,41 @@
 package home;
 
 public class LivingRoom {
+    private int numberOfTvs;
+    private String livingRoomType;
+
+
+    public LivingRoom(){
+        this.numberOfTvs = 0;
+        this.livingRoomType = "unknown";
+    }
+
+    public LivingRoom(int numberOfTvs, String livingRoomType) {
+        this.numberOfTvs = numberOfTvs;
+        this.livingRoomType = livingRoomType;
+    }
+
+    public int getNumberOfTvs() {
+        return numberOfTvs;
+    }
+
+    public void setNumberOfTvs(int numberOfTvs) {
+        this.numberOfTvs = numberOfTvs;
+    }
+
+    public String getLivingRoomType() {
+        return livingRoomType;
+    }
+
+    public void setLivingRoomType(String livingRoomType) {
+        this.livingRoomType = livingRoomType;
+    }
+
+    @Override
+    public String toString() {
+        return "LivingRoom{" +
+                "numberOfTvs=" + numberOfTvs +
+                ", livingRoomType='" + livingRoomType + '\'' +
+                '}';
+    }
 }
