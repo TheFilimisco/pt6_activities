c85f8f0599323110d95a7f45ebef92c5ce1b61e2
TheFilimisco <54910978l@iespoblenou.org>
2024-12-12 16:36:54 +0100

    finish hotel class

--- src/hotelreservationmanagement/models/Hotel.java
+++ src/hotelreservationmanagement/models/Hotel.java
@@ -18,15 +18,13 @@ public class Hotel {
         rooms.add(new Room("simple"));
         rooms.add(new Room("simple"));
         rooms.add(new Room("simple"));
-
         rooms.add(new Room("double"));
         rooms.add(new Room("double"));
-
         rooms.add(new Room("suite"));
         this.reservations = new ArrayList<>();
     }
 
-    public Hotel(Room room){
+    public Hotel(Room room) {
         rooms = new ArrayList<>();
         rooms.add(room);
     }
@@ -48,26 +46,58 @@ public class Hotel {
         this.reservations = reservations;
     }
 
-    public void addNewReservation(String typeOfRoomYouWant, String nameClient, LocalDate checkInDate, LocalDate checkOutDate){
-        for (Room room: rooms){
-            if (room.getTypeOfRoom().equals(typeOfRoomYouWant)){
-                reservations.add(new Reservation(nameClient,checkInDate,checkOutDate,room));
+    public void addNewReservation(String typeOfRoomYouWant, String nameClient, LocalDate checkInDate, LocalDate checkOutDate) {
+        for (Room room : rooms) {
+            if (room.getTypeOfRoom().equals(typeOfRoomYouWant)) {
+                reservations.add(new Reservation(nameClient, checkInDate, checkOutDate, room));
+                break;
             }
         }
     }
 
-    public void searchRoomsAvailableForDate(LocalDate checkInDate, LocalDate checkOutDate){
+    public void searchRoomsAvailableForDate(LocalDate checkInDate, LocalDate checkOutDate) {
         var numberRoomNotAvailable = 0;
 
-        for (Reservation reservation: reservations) {
-            if (!(reservation.getCheckInDate().isAfter(checkInDate) || reservation.getCheckOutDate().isBefore(checkOutDate))){
+        for (Reservation reservation : reservations) {
+            if (!(reservation.getCheckInDate().isAfter(checkInDate) || reservation.getCheckOutDate().isBefore(checkOutDate))) {
                 numberRoomNotAvailable = reservation.getRoom().getNumberRoom();
             }
         }
-        for (Room room: rooms){
-            if (!(room.getNumberRoom()==numberRoomNotAvailable)){
+        for (Room room : rooms) {
+            if (!(room.getNumberRoom() == numberRoomNotAvailable)) {
                 System.out.println(room);
             }
+        }
+    }
+
+    public void itsRoomAvailableForDate(int inputYourNumberRoom, LocalDate specificDate) {
+        for (Reservation reservation : reservations) {
+            if ((reservation.getCheckInDate().isBefore(specificDate) && reservation.getCheckOutDate().isAfter(specificDate)) && inputYourNumberRoom == reservation.getRoom().getNumberRoom()) {
+                System.out.println(reservation.getRoom() + " this Reserved");
+            } else {
+                System.out.println(reservation.getRoom() + " is not Reserved");
+            }
+        }
+    }
+
+    public void getServicesOfRoom(int inputYourNumberRoom) {
+        for (Room room : rooms) {
+            if (room.getNumberRoom() == inputYourNumberRoom) {
+                System.out.println("The room: " + room.getNumberRoom() + " has: " + room.getServices());
+            }
+        }
+    }
+
+
+    public void showTotalRooms() {
+        for (Room room : rooms) {
+            System.out.println(room);
+        }
+    }
+
+    public void showTotalReservations() {
+        for (Reservation reservation : reservations) {
+            System.out.println(reservation);
         }
     }
 
--- src/hotelreservationmanagement/test/TestHotel.java
+++ src/hotelreservationmanagement/test/TestHotel.java
@@ -16,29 +16,32 @@ public class TestHotel {
     public static void main(String[] args) {
 
 
-
         Hotel hotel = new Hotel();
         Scanner input = new Scanner(System.in);
         var running = true;
 
         while (running) {
             System.out.println("""
-                ==================Menu Hotel==============
-                1. Add to reservation to Hotel
-                2. Get valuables rooms
-                5. Close
-                """);
+                    ==================Menu Hotel==============
+                    1. Add to reservation to Hotel
+                    2. Get Available rooms
+                    3. Get services a room
+                    4. Show Rooms a Hotel with Reservations
+                    5. Its available this room?
+                    6. Close
+                    ==========================================
+                    """);
             System.out.print("Enter your number: ");
             var numberInput = input.nextInt();
-            switch (numberInput){
+            switch (numberInput) {
                 case 1:
                     input.nextLine();
                     System.out.println("===========Add Reservation========");
 
-                    System.out.print("Whats your name: ");
+                    System.out.print("Enter your name: ");
                     String inputYourName = input.nextLine();
 
-                    System.out.print("What kind of room you wish: " );
+                    System.out.print("What kind of room you wish: ");
                     String inputKindOfRoom = input.nextLine();
 
                     System.out.println("Put the Check-in-date(YYYY-MM-DD): ");
@@ -48,7 +51,7 @@ public class TestHotel {
                     System.out.println("Put the Check-out-date(YYYY-MM-DD): ");
                     String inputDateTimeSecond = input.nextLine();
                     LocalDate dateSecond = LocalDate.parse(inputDateTimeSecond, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-                    hotel.addNewReservation(inputKindOfRoom,inputYourName,dateFirst,dateSecond);
+                    hotel.addNewReservation(inputKindOfRoom, inputYourName, dateFirst, dateSecond);
                     break;
                 case 2:
                     input.nextLine();
@@ -60,9 +63,35 @@ public class TestHotel {
                     System.out.println("Put the Check-out-date(YYYY-MM-DD): ");
                     String searchSecondtDate = input.nextLine();
                     LocalDate secondDate = LocalDate.parse(searchSecondtDate, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-                    hotel.searchRoomsAvailableForDate(firstDate,secondDate);
+                    hotel.searchRoomsAvailableForDate(firstDate, secondDate);
+                    break;
+                case 3:
+                    input.nextLine();
+                    System.out.println("=================Show Services of a Room=========");
+                    System.out.println("Enter your room number: ");
+                    int inputNumberRoom = input.nextInt();
+                    hotel.getServicesOfRoom(inputNumberRoom);
+                    break;
+                case 4:
+                    input.nextLine();
+                    System.out.println("=================Rooms of your Hotel=========");
+                    hotel.showTotalRooms();
+                    System.out.println("=================Reservations of your Hotel=========");
+                    hotel.showTotalReservations();
                     break;
                 case 5:
+                    input.nextLine();
+                    System.out.println("============Its available this room?==============");
+                    System.out.print("Enter your room number: ");
+                    int inputSearchRoom = input.nextInt();
+                    input.nextLine();
+                    System.out.println("Put the Date (YYYY-MM-DD): ");
+                    String inputSpecificDate = input.nextLine();
+
+                    LocalDate specificDate = LocalDate.parse(inputSpecificDate, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+                    hotel.itsRoomAvailableForDate(inputSearchRoom, specificDate);
+                    break;
+                case 6:
                     System.out.println("Leave...");
                     running = false;
                     break;
@@ -72,11 +101,7 @@ public class TestHotel {
         }
 
 
-
     }
-
-
-
 
 
 }
